2a5175761cfb9acfb83dc26d459fc8bed654743f
Maksim Tkashev <maks93makula@gmail.com>
2024-03-26 15:48:28 +0300

    Измена верстка MUI, мелкие недочеты и скругления, изменил цвет на более интересный

--- front/src/components/Chat/Chat.tsx
+++ front/src/components/Chat/Chat.tsx
@@ -10,28 +10,38 @@ interface ChatProps {
 
 const Chat: React.FC<ChatProps> = ({ messages, onSubmit }) => {
     return (
-        <Container maxWidth="sm">
-        <div style={{ maxHeight: '300px', overflowY: 'auto', padding: '10px', border: '1px solid #ccc', borderRadius: '4px', marginBottom: '10px', width: '100%' }}>
-          {messages.map((message) => (
-            <Message key={message.id} message={message} />
-          ))}
-        </div>
-        <form
-          onSubmit={(e: React.FormEvent<HTMLFormElement>) => {
-            e.preventDefault();
-            const formData = new FormData(e.currentTarget);
-            onSubmit(formData.get('author') as string, formData.get('message') as string);
-            e.currentTarget.reset();
-          }}
-          style={{ width: '100%' }}
-        >
-          <TextField name="author" label="Ваше имя" variant="outlined" size="small" required fullWidth />
-          <TextField name="message" label="Ваше сообщение" variant="outlined" size="small" required fullWidth />
-          <Button type="submit" variant="contained" color="primary" fullWidth>
-            Отправить
-          </Button>
-        </form>
-      </Container>
+      <Container maxWidth="sm">
+      <div style={{ maxHeight: '300px', overflowY: 'auto', padding: '20px', border: '1px solid #b2dfdb', borderRadius: '4px', marginBottom: '20px', width: '100%', backgroundColor: '#e0f2f1'
+      }}>
+        {messages.map((message) => (
+          <Message key={message.id} message={message} />
+        ))}
+      </div>
+
+      <form
+        onSubmit={(e: React.FormEvent<HTMLFormElement>) => {
+          e.preventDefault();
+          const formData = new FormData(e.currentTarget);
+          onSubmit(formData.get('author') as string, formData.get('message') as string);
+          e.currentTarget.reset();
+        }}
+        style={{ width: '100%' }}
+      >
+        <TextField name="author" label="Ваше имя" variant="outlined" size="small" required fullWidth margin="normal" style={{ marginBottom: '10px' }}
+        />
+        <TextField name="message" label="Ваше сообщение" variant="outlined" size="small" required fullWidth margin="normal" style={{ marginBottom: '20px' }}
+        />
+        <Button type="submit" variant="contained" fullWidth 
+        style={{ 
+            backgroundColor: '#26a69a',
+            color: 'white',
+            padding: '10px 0',
+            borderRadius: '4px',
+          }}>
+          Отправить
+        </Button>
+      </form>
+    </Container>
     );
 };
 
